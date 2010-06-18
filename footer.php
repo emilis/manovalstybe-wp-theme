0af1d189e4a5fa9a622bd4d6435a019488930509
Emilis Dambauskas <emilis.d@gmail.com>
2010-06-18 11:59:19 +0300

    Initial commit.

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+	<div id="ft">	
+	<p>(cc) <?php echo date("Y") ?> <a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a> <?php if ( "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == get_option('home')."/" || "http://www.".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == get_option('home')."/" || $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == get_option('home')."/" || "www.".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] == get_option('home')."/" ) : ?>
+	<?php endif; ?>| Some Rights Reserved</p>
+	<p><?php wp_footer() ?></p>
+	</div>
+
+</div>
+
+<!-- Can put web stats code here -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/reset-fonts-grids.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet directory'); ?>/base.css">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/reset.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<!--[if IE]><link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/ie.css" type="text/css" media="screen" /><![endif]-->
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php wp_head(); ?>
+
+</head>
+
+<body>
+<div id="doc3" class="yui-t5">
+	<div id="hd">
+		<div id="logo">
+		<a href="<?php echo get_option('home'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/mv_ongreen.jpg" alt="<?php bloginfo('name'); ?>" border="0"></a>
+		<!--<h2><?php bloginfo('description'); ?></h2>-->
+		</div>
+		<div id="nav">
+		<?php if ( !function_exists('dynamic_sidebar')
+        || !dynamic_sidebar('Top Navigation') ) : ?>
+			<ul>
+				<?php wp_list_pages('title_li=&depth=1'); ?>
+			</ul>
+		<?php endif; ?>
+		</div>
+	</div>
+
